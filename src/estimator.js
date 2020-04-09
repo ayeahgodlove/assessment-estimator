441ac9a25c27b5a7a89c65c35b1d7f7a9f67ebbc
Ayeah Godlove <ayeahgodlove5@gmail.com>
2020-04-09 17:27:59 +0100

    update

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const inputData = data;
+  let currentlyInfected;
+  return {
+    data: inputData,
+    impact: {
+      currentlyInfected: inputData.reportedCases * 10,
+      infectionsByRequestedTime: currentlyInfected * (2 ** 10)
+
+    },
+    servereImpact: {
+      currentlyInfected: inputData.reportedCases * 50,
+      infectionsByRequestedTime: currentlyInfected * (2 ** 10)
+
+    }
+  };
+};
+
 
 export default covid19ImpactEstimator;
