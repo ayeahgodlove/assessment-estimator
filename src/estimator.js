ed5fc760cdbd0c0d786b3e3864ca22caa490a398
Ayeah Godlove <ayeahgodlove5@gmail.com>
2020-04-11 04:31:44 +0100

    estimator.js updated

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,41 @@
+/*
+  data = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  };
+*/
 const covid19ImpactEstimator = (data) => {
   const inputData = data;
   let currentlyInfected;
+  /*
+   impact Object
+  */
+  let impact = {};
+  impact = {
+    currentlyInfected: inputData.reportedCases * 10,
+    infectionsByRequestedTime: currentlyInfected * (2 ** 10)
+  };
+  /*
+   serverImpact Object
+  */
+  let servereImpact = {};
+  servereImpact = {
+    currentlyInfected: inputData.reportedCases * 50,
+    infectionsByRequestedTime: currentlyInfected * (2 ** 10)
+  };
   return {
-    data: inputData,
-    impact: {
-      currentlyInfected: inputData.reportedCases * 10,
-      infectionsByRequestedTime: currentlyInfected * (2 ** 10)
-
-    },
-    servereImpact: {
-      currentlyInfected: inputData.reportedCases * 50,
-      infectionsByRequestedTime: currentlyInfected * (2 ** 10)
-
-    }
+    inputData,
+    impact,
+    servereImpact
   };
 };
 
