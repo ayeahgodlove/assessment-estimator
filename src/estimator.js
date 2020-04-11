29ee44d8481f11f3f06c419a2f7af8f6fd51576b
Ayeah Godlove <ayeahgodlove5@gmail.com>
2020-04-11 06:11:45 +0100

    latest update

--- src/estimator.js
+++ src/estimator.js
@@ -12,14 +12,39 @@
     population: 66622705,
     totalHospitalBeds: 1380614
   };
+  1w - 7ds
+  4ws - 7 * 4 = 28
+
+  1m - 30ds
+  4ms = x =4*30
+  toDays converter
 */
 const covid19ImpactEstimator = (data) => {
   const inputData = data;
   let currentlyInfected;
+  let impact = {};
+  let servereImpact = {};
+  /*
+    check periodType: if days,weeks or months
+  */
+  let toDays = inputData.timeToElapse;
+  if (inputData.periodType === 'days') {
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** (toDays / 3));
+    servereImpact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** (toDays / 3));
+  }
+  if (inputData.periodType === 'weeks') {
+    toDays = inputData.timeToElapse * 7;
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** (toDays / 3));
+    servereImpact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** (toDays / 3));
+  }
+  if (inputData.periodType === 'months') {
+    toDays = inputData.timeToElapse * 30;
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** (toDays / 3));
+    servereImpact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** (toDays / 3));
+  }
   /*
    impact Object
   */
-  let impact = {};
   impact = {
     currentlyInfected: inputData.reportedCases * 10,
     infectionsByRequestedTime: currentlyInfected * (2 ** 10)
@@ -27,7 +52,6 @@ const covid19ImpactEstimator = (data) => {
   /*
    serverImpact Object
   */
-  let servereImpact = {};
   servereImpact = {
     currentlyInfected: inputData.reportedCases * 50,
     infectionsByRequestedTime: currentlyInfected * (2 ** 10)
@@ -38,6 +62,5 @@ const covid19ImpactEstimator = (data) => {
     servereImpact
   };
 };
-
 
 export default covid19ImpactEstimator;
